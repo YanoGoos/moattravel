208e50ad2ddcea243010cffd22097e8262377d73
YanoGoos <kotomi.yano@goos.jp>
2026-07-03 18:05:42 +0900

    ～26/07/03 18:00

---
+++ src/main/java/com/example/moattravel/repository/HouseRepository.java
@@ -0,0 +1,16 @@
+package com.example.moattravel.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.moattravel.entity.House;
+/*
+public interface HouseRepository {
+*/
+	public interface HouseRepository extends JpaRepository<House, Integer> {
+		public Page<House> findBynameLike(String keyword, Pageable pageable);
+	}
+/*
+}
+*/
