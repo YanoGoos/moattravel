72aeca13d8c8c46d8b56c17f6e6a18edcca96704
YanoGoos <kotomi.yano@goos.jp>
2026-07-14 17:57:20 +0900

    ～26/07/14 18:00

--- src/main/java/com/example/moattravel/service/UserService.java
+++ src/main/java/com/example/moattravel/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.moattravel.entity.Role;
 import com.example.moattravel.entity.User;
 import com.example.moattravel.form.SignupForm;
+import com.example.moattravel.form.UserEditForm;
 import com.example.moattravel.repository.RoleRepository;
 import com.example.moattravel.repository.UserRepository;
 
@@ -35,10 +36,22 @@ public class UserService {
 		user.setEmail(signupForm.getEmail());
 		user.setPassword(passwordEncoder.encode(signupForm.getPassword()));
 		user.setRole(role);
-		//user.setEnabled(true);
 		user.setEnabled(false);
 		
 		return userRepository.save(user);
+	}
+	
+	@Transactional
+	public void update(UserEditForm userEditForm) {
+		User user = userRepository.getReferenceById(userEditForm.getId());
+		user.setName(userEditForm.getName());
+		user.setFurigana(userEditForm.getFurigana());
+		user.setPostalCode(userEditForm.getPostalCode());
+		user.setAddress(userEditForm.getAddress());
+		user.setPhoneNumber(userEditForm.getPhoneNumber());
+		user.setEmail(userEditForm.getEmail());
+		
+		userRepository.save(user);
 	}
 	
 	//メールアドレスが登録済みかどうかをチェックする
@@ -57,5 +70,11 @@ public class UserService {
 	public void enableUser(User user) {
 		user.setEnabled(true);
 		userRepository.save(user);
+	}
+	
+	//メールアドレスが変更されたかどうかチェックする
+	public boolean isEmailChanged(UserEditForm userEditForm) {
+		User currentUser = userRepository.getReferenceById(userEditForm.getId());
+		return !userEditForm.getEmail().equals(currentUser.getEmail());
 	}
 }
